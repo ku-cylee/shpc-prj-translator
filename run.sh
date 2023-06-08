154ab44b412a28795ce574191e476e0469637c25
Chanyoung Lee <btstar1998@gmail.com>
2023-06-09 01:27:15 +0900

    Update run.sh to use 4 nodes

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-: ${NODES:=1}
+: ${NODES:=4}
 
-srun -N $NODES --exclusive --partition=shpc          \
+salloc -N $NODES --exclusive --partition=shpc          \
   mpirun --bind-to none -mca btl ^openib -npernode 1 \
   numactl --physcpubind 0-63                         \
   /usr/local/cuda/bin/ncu --set full ./translator $@
